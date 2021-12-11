7982820fa55eacc8700b80569e62dda244f68f9d
Giorgos Stefanakis <stefgs99@gmail.com>
2021-12-11 22:47:35 +0200

    Using mysql2 for Promise based methods

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,14 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 require('dotenv').config();
 
 // Export MySQL connection pool with 10
 // concurrently available connections
-module.exports = mysql.createPool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    connectionLimit: 10,
-    host: process.env.HOST,
-    database: process.env.DB_NAME
-});
+module.exports = mysql
+    .createPool({
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        connectionLimit: 10,
+        host: process.env.HOST,
+        database: process.env.DB_NAME
+    })
+    .promise();
