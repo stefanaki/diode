c76f6ba2a35a0b7cd5c36aded9aaa3a10cdda3b1
Giorgos Stefanakis <stefgs99@gmail.com>
2021-12-13 03:37:33 +0200

    Correct API endpoints and new routes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,9 @@
 const express = require('express');
-const PassesRouter = require('./routes/Passes');
+const PassesRouter = require('./routes/PassesRoutes');
+const AuthRouter = require('./routes/AuthRoutes');
+const AdminRouter = require('./routes/AdminRoutes');
+
+const auth = require('./middleware/auth');
 
 require('dotenv').config();
 
@@ -8,7 +12,13 @@ const port = process.env.PORT;
 
 app.use(express.json());
 
-app.use('/interoperability/', PassesRouter);
+app.post('/welcome', auth, (req, res) =>
+    res.status(200).json({ message: 'Congrats, user system works correctly', user: req.user })
+);
+
+app.use('/interoperability/api/', AuthRouter);
+app.use('/interoperability/api/admin', auth, AdminRouter);
+app.use('/interoperability/api/', PassesRouter);
 app.use('*', (req, res) => res.status(404).json({ message: 'Bad request: Endpoint not found' }));
 
-app.listen(port, () => console.log(`it's alive on http://localhost:${port}`));
+app.listen(port, () => console.log(`It's alive on http://localhost:${port}`));
