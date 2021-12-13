efd869559c1c9e5b40ac8243f5ee480985591e08
Giorgos Stefanakis <stefgs99@gmail.com>
2021-12-13 03:42:50 +0200

    Rename

--- backend/routes/Passes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const PassesPerStation = require('../controllers/PassesPerStation');
-
-// GET all passes for specified station and time period
-router.get('/PassesPerStation/:stationID/:dateFrom/:dateTo', PassesPerStation);
-
-module.exports = router;
