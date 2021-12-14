338edce7c68e0f9a8a2189d22e457b524818676c
Giorgos Stefanakis <stefgs99@gmail.com>
2021-12-14 03:58:09 +0200

    Connection string

--- backend/controllers/HealthCheck.js
+++ backend/controllers/HealthCheck.js
@@ -2,17 +2,29 @@ const pool = require('./../config/db');
 
 module.exports = async (req, res) => {
     let { DB_NAME, DB_HOST, DB_USER, DB_PASSWORD, DB_POOL_SIZE } = process.env;
+    let connectionString =
+        'mysql;host=' +
+        DB_HOST +
+        ';db_name=' +
+        DB_NAME +
+        ';user=' +
+        DB_USER +
+        ';password=' +
+        DB_PASSWORD +
+        ';connection_limit=' +
+        DB_POOL_SIZE;
 
     try {
-        await pool.getConnection();
+        const connection = await pool.getConnection();
+        connection.release();
         res.status(200).json({
             status: 'OK',
-            dbconnection: { DB_NAME, DB_HOST, DB_USER, DB_PASSWORD, DB_POOL_SIZE }
+            dbconnection: connectionString
         });
     } catch (error) {
         res.status(500).json({
             status: 'Failed',
-            dbconnection: { DB_NAME, DB_HOST, DB_USER, DB_PASSWORD, DB_POOL_SIZE }
+            dbconnection: connectionString
         });
     }
 };
