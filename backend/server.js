84be111c275149f9cf8e56201f0e342571c55745
Giorgos Stefanakis <stefgs99@gmail.com>
2021-12-18 03:48:54 +0200

    URL encoded body parameters

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/interoperability/api/', AuthRouter);
 app.use('/interoperability/api/admin', auth, AdminRouter);
