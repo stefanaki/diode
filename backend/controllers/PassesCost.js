3581e38e02e12a92042c7410872405c0b4433a49
Giorgos Stefanakis <stefgs99@gmail.com>
2021-12-20 21:38:31 +0200

    Fixed query mistakes

--- backend/controllers/PassesCost.js
+++ backend/controllers/PassesCost.js
@@ -3,50 +3,44 @@ const moment = require('moment');
 const sendResponse = require('../utilities/sendFormattedResponse');
 
 module.exports = async (req, res) => {
-    const { op1_ID , op2_ID, date_from, date_to } = req.params;
+    const { op1_ID, op2_ID, date_from, date_to } = req.params;
     const format = 'YYYY-MM-DD HH:mm:ss';
     const dateTimeNow = moment().format(format);
-    
 
-    const finalQuery = `
-SELECT Count(pass_id) as NumberOfPasses , SUM(pass_charge) as PassesCost
-FROM passes p 
-INNER JOIN stations s
-ON p.station_id = s.st_id AND s.op_name = ?
-INNER JOIN tags t
-ON t.tag_id = p.tag_id AND tag_provider = ?
-WHERE (pass_timestamp
-BETWEEN ? AND ?)
-ORDER BY p.pass_timestamp ASC;
-            `;
- 
+    const query = `
+        SELECT count(pass_id) as NumberOfPasses, sum(pass_charge) as PassesCost
+        FROM passes p 
+        INNER JOIN stations s
+        ON p.station_id = s.st_id AND s.op_name = ?
+        INNER JOIN tags t
+        ON t.tag_id = p.tag_id AND tag_provider = ?
+        WHERE pass_timestamp BETWEEN ? AND ?`;
 
     try {
         const connection = await pool.getConnection();
-        const finalQueryRes = await connection.query(finalQuery, [op1_ID,date_from,date_to]);
+        const queryRes = await connection.query(query, [op1_ID, op2_ID, date_from, date_to]);
 
-        if (!finalQueryRes[0][0]) {
+        if (!queryRes[0][0]) {
             return sendResponse(req, res, 400, {
                 message: 'Bad request: Invalid op_ID'
             });
         }
 
-
         // Parse result as JS object and include PPOList
-        let queryResultList = JSON.parse(JSON.stringify(finalQueryRes));
+        let queryResultList = JSON.parse(JSON.stringify(queryRes));
         sendResponse(req, res, 200, {
             op1_ID: op1_ID,
-            op2_ID : op2_ID,
+            op2_ID: op2_ID,
             RequestTimestamp: dateTimeNow,
             PeriodFrom: date_from,
             PeriodTo: date_to,
-            NumberOfPasses: finalQueryRes[0][0],
-            PassesCost: finalQuery[0][1]
+            NumberOfPasses: queryRes[0][0],
+            PassesCost: query[0][1]
         });
-    
+
         connection.release();
-       
-    } catch {
+    } catch (error) {
+        console.log(error);
         sendResponse(req, res, 500, { message: 'Internal server error' });
     }
 };
