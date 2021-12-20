d14f971d3f1796204f0cc4bfb0060c3f15de634c
Giorgos Stefanakis <stefgs99@gmail.com>
2021-12-20 17:01:13 +0200

    Responding now with JSON/CSV

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,26 +1,27 @@
 const jwt = require('jsonwebtoken');
 const redisClient = require('../config/redis');
+const sendResponse = require('../utilities/sendFormattedResponse');
 
 module.exports = async (req, res, next) => {
     const token = req.header('X-OBSERVATORY-AUTH');
 
-    if (!token) return res.status(403).json({ message: 'Authentication token required' });
+    if (!token) return sendResponse(req, res, 403, { message: 'Authentication token required' });
 
     try {
         let isBlacklisted = await redisClient.lPos('blacklisted_tokens', token);
         if (isBlacklisted)
-            return res
-                .status(401)
-                .json({ message: 'User with specified token has logged out. Please log in.' });
+            return sendResponse(req, res, 401, {
+                message: 'User with specified token has logged out. Please log in.'
+            });
     } catch (error) {
-        return res.status(500).json({ message: 'Internal server error' });
+        return sendResponse(req, res, 500, { message: 'Internal server error' });
     }
 
     try {
         const decoded = jwt.verify(token, process.env.TOKEN_KEY);
         req.user = decoded;
     } catch (error) {
-        return res.status(401).json({ message: 'Invalid token' });
+        return sendResponse(req, res, 401, { message: 'Invalid token' });
     }
     return next();
 };
