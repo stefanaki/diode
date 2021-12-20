b8af7c8b85d019cc9bb8ec86f4fe7ca555aecf9e
Odysseas Boufalis <ody.boufalis@gmail.com>
2021-12-20 13:45:38 +0200

    Added ChargesBy router

--- backend/routes/ChargesByRoutes.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const ChargesBy = require('../controllers/ChargesBy');
-
-router.get('/ChargesBy/:opID/:date_From/:date_To', ChargesBy);
-
-module.exports = router;
--- backend/routes/PassesRoutes.js
+++ backend/routes/PassesRoutes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const PassesPerStation = require('../controllers/PassesPerStation');
+const ChargesBy = require('../controllers/ChargesBy');
 
+router.get('/ChargesBy/:opID/:date_From/:date_To', ChargesBy);
 router.get('/PassesPerStation/:stationID/:dateFrom/:dateTo', PassesPerStation);
 
 module.exports = router;
