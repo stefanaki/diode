e5c050b2a7d41e5655de9c888511c1aa8d2f4f87
Giorgos Stefanakis <stefgs99@gmail.com>
2021-12-27 01:30:11 +0200

    Connection log

--- backend/config/redis.js
+++ backend/config/redis.js
@@ -2,6 +2,7 @@ const redis = require('redis').createClient(process.env.REDIS_PORT);
 (async () => {
     try {
         await redis.connect();
+        console.log(`Connected to Redis server on port ${process.env.REDIS_PORT}`);
     } catch (error) {
         console.error(error);
     }
