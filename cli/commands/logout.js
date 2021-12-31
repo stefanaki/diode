7e8865b133b7ac1232f832a23f8ceae746819459
johnpalaios <johnpalaios123@gmail.com>
2021-12-31 19:53:48 +0200

    Added 4 cli commands

---
+++ cli/commands/logout.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+const fs = require('fs');
+
+module.exports = async () => {
+    try {
+        if (!fs.existsSync('.token')) return console.log('Please log in first.');
+        const token = fs.readFileSync('.token', 'utf8');
+
+        if (token.length === 0) return console.log('Please log in first.');
+
+        const logout = await axios({
+            url: 'https://localhost:9103/interoperability/api/logout',
+            method: 'post',
+            headers: {
+                'X-OBSERVATORY-AUTH': token
+            },
+            httpsAgent: new require('https').Agent({ rejectUnauthorized: false })
+        });
+
+        fs.unlinkSync('.token');
+        console.log(`Log out successful.`);
+    } catch (error) {
+        console.log('Something went wrong...');
+        console.log(error.response.status, error.response.data);
+    }
+};
---
+++ cli/commands/resetPasses.js
@@ -0,0 +1,25 @@
+const axios = require('axios');
+const fs = require('fs');
+
+module.exports = async () => {
+    try {
+        if (!fs.existsSync('.token')) return console.log('Please log in first.');
+        const token = fs.readFileSync('.token', 'utf8');
+
+        if (token.length === 0) return console.log('Please log in first.');
+
+        const resetPasses = await axios({
+            url: 'https://localhost:9103/interoperability/api/admin/resetpasses',
+            method: 'post',
+            headers: {
+                'X-OBSERVATORY-AUTH': token
+            },
+            httpsAgent: new require('https').Agent({ rejectUnauthorized: false })
+        });
+
+        console.log(resetPasses.data);
+    } catch (error) {
+        console.log('Something went wrong...');
+        console.log(error.response.status, error.response.data);
+    }
+};
