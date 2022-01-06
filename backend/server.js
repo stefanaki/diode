38e484435d67a782f9c13ecf102644112d2315b4
Giorgos Stefanakis <stefgs99@gmail.com>
2022-01-06 21:09:10 +0200

    Typo fix

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const https = require('https');
 const fs = require('fs');
-const PassesRouter = require('./routes/PassesRoutes');
+const PassRouter = require('./routes/PassRoutes');
 const AuthRouter = require('./routes/AuthRoutes');
 const AdminRouter = require('./routes/AdminRoutes');
 const SettlementRouter = require('./routes/SettlementRoutes');
@@ -26,7 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/interoperability/api/', AuthRouter);
 app.use('/interoperability/api/settlements', auth('user'), SettlementRouter);
 app.use('/interoperability/api/admin', auth('admin'), AdminRouter);
-app.use('/interoperability/api/', auth('user'), PassesRouter);
+app.use('/interoperability/api/', auth('user'), PassRouter);
 app.use('*', (req, res) => res.status(404).json({ message: 'Bad request: Endpoint not found' }));
 
 server.listen(port, () => console.log(`It's alive on https://localhost:${port}`));
