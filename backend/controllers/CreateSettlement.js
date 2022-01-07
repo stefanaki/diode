ba46cd24eeecbfd38c46241e35aeddc0282d0e99
Odysseas Boufalis <ody.boufalis@gmail.com>
2022-01-07 20:53:07 +0200

    Added new endpoint CreateSettlement

---
+++ backend/controllers/CreateSettlement.js
@@ -0,0 +1,77 @@
+const pool = require('./../config/db');
+const sendResponse = require('../utilities/sendFormattedResponse');
+
+module.exports = async (req, res) => {
+    const { op1_ID, op2_ID, date_from, date_to } = req.body;
+    
+    if (op1_ID == op2_ID) {
+        return sendResponse(req, res, 400, {
+            message: `Bad request: The 2 Operators IDs are the same`
+        });
+    }
+    
+    const checkQuery = `
+        SELECT operator_credited, operator_debited, date_from, date_to 
+        FROM settlements 
+        WHERE (
+         ( (operator_credited = ? AND operator_debited = ?) OR (operator_credited = ? AND operator_debited = ? ) ) 
+         AND 
+         ( (date_from >= ? AND date_from <= ? ) OR (date_to >= ? AND date_from <= ?) )   
+        )`;
+
+    // Compute the amount of the settlement and who is credited and who is debited
+    const computequery = `
+    SELECT SUM(pass_charge) AS PassesCost
+    FROM passes p 
+    INNER JOIN stations s
+    ON p.station_id = s.st_id AND s.op_name = ?
+    INNER JOIN tags t
+    ON t.tag_id = p.tag_id AND tag_provider = ?
+    WHERE pass_timestamp BETWEEN ? AND ?
+    `;
+
+    const insertquery = `
+    INSERT INTO settlements (operator_credited,operator_debited,date_from,date_to,amount,status)
+    VALUES (?,?,?,?,?,?)
+     `;
+
+    try {
+        const connection = await pool.getConnection();
+
+        let check = await connection.query(checkQuery, [op1_ID,op2_ID,op2_ID,op1_ID,date_from,date_to,date_from,date_from]);
+
+        if (check[0][0]) {
+            return sendResponse(req, res, 400, {
+                message: 'Bad request: The settlement overlaps with existing settlement'
+            });
+        }
+        
+
+        let q1 = await connection.query(computequery, [op1_ID, op2_ID, date_from, date_to]); // how much money does op2 owes to op1
+        let q2 = await connection.query(computequery, [op2_ID, op1_ID, date_from, date_to]); // how much money does op1 owes to op2
+        
+        let amount = Math.abs(q2[0][0].PassesCost - q1[0][0].PassesCost);
+        let status = 0;
+        
+       
+        if(q1[0][0].PassesCost < q2[0][0].PassesCost) { // op1 is debited and op2 is credited
+            await connection.query(insertquery,[op2_ID,op1_ID,date_from,date_to,amount,status]);    
+        } 
+        if (q1[0][0].PassesCost > q2[0][0].PassesCost) { // op2 is debited and op1 is credited
+            await connection.query(insertquery,[op1_ID,op2_ID,date_from,date_to,amount,status]);
+        }
+        if (q1[0][0].PassesCost == q2[0][0].PassesCost){
+            return sendResponse(req, res, 200, {
+                message: 'The settlement amount is zero'
+            });
+        }
+
+        connection.release();
+        sendResponse(req, res, 200, {
+            message: 'Settlement has been created successfully!'
+        });
+    } catch (error) {
+        console.log(error);
+        sendResponse(req, res, 500, { message: 'Internal server error' });
+    }
+};
--- backend/routes/SettlementRoutes.js
+++ backend/routes/SettlementRoutes.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
+const CreateSettlement = require('../controllers/CreateSettlement');
 const VerifySettlement = require('../controllers/VerifySettlement');
 const auth = require('../middleware/auth');
+const checkDateFormats = require('../middleware/checkDateFormats')
 
+router.post('/CreateSettlement', [auth('admin'),checkDateFormats], CreateSettlement)
 router.post('/VerifySettlement', auth('bank'), VerifySettlement);
 module.exports = router;
