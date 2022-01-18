06a991239d71ac225b264e202aa5620cf97503c3
Giorgos Stefanakis <stefgs99@gmail.com>
2022-01-19 00:27:32 +0200

    Corrected CSV response format

--- backend/utilities/sendFormattedResponse.js
+++ backend/utilities/sendFormattedResponse.js
@@ -1,14 +1,21 @@
 const { parse } = require('json2csv');
 
 module.exports = (req, res, status, data) => {
-    try {
-        const format = req.query.format;
-        if (!format || format === 'json') return res.status(status).json(data);
-        if (format !== 'csv') return res.status(400).json({ message: 'Invalid format option' });
+	try {
+		const format = req.query.format;
+		if (!format || format === 'json') return res.status(status).json(data);
+		if (format !== 'csv') return res.status(400).json({ message: 'Invalid format option' });
 
-        let dataArray = parse(data);
-        res.attachment('response_data.csv').send(dataArray);
-    } catch (error) {
-        res.status(500).json({ message: 'Internal server error' });
-    }
+		for (const member of Object.values(data)) {
+			if (Array.isArray(member)) {
+				let list = parse(member);
+				return res.attachment('response_data.csv').send(list);
+			}
+		}
+
+		let dataArray = parse(data);
+		res.attachment('response_data.csv').send(dataArray);
+	} catch (error) {
+		res.status(500).json({ message: 'Internal server error' });
+	}
 };
